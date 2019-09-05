40fbd4f86282d938c620159ea9d3972c2e8a3139
Marcelo Alves <marceloalves_ac@hotmail.com>
2019-09-05 08:59:11 -0300

    landing-page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import './App.css'
 import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
+import {Link} from 'react-router-dom'
+
+import Main from './components/main'
 
 function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header title="Portifólio Site" scroll>
+        <Header className="header-color" title="Portifólio Site" scroll>
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/">Inicio</Link>
+                <Link to="/resume">Resumo</Link>
+                <Link to="/aboutme">Sobre Mim</Link>
+                <Link to="/projects">Projetos</Link>
+                <Link to="/contact">Contato</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resumo</Link>
+                <Link to="/aboutme">Sobre Mim</Link>
+                <Link to="/projects">Projetos</Link>
+                <Link to="/contact">Contato</Link>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main/>
         </Content>
     </Layout>
 </div>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl'
 
 class LandingPage extends Component {
     render () {
         return(
-            <div>
-                <h1>LandingPage</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img className="avatar-img"
+                        alt="avatar"
+                        src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-512.png"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr/>
+
+                            <p>Html/Css - Bootstrap - Javascript - React - React Native - Nodejs - Express - MongoDb</p>
+
+                            <div className="social-links">
+                                <a href="www.google.com" rel="noopener noreferrer" target="_blank"> <i className="fa fa-linkedin-square" aria-hidden="true"></i> </a>
+                                <a href="www.google.com" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"></i> </a>
+                                <a href="www.google.com" rel="noopener noreferrer" target="_blank"> <i className="fa fa-free-code-camp" aria-hidden="true"></i> </a>
+                                <a href="www.google.com" rel="noopener noreferrer" target="_blank"> <i className="fa fa-youtube-square" aria-hidden="true"></i> </a>
+                            </div>
+
+                        </div>
+
+                    </Cell>
+                </Grid>
             </div>
             
         )
